cf8b4b255e080c9896e4ef818521c44e5486cecd
Tim Gangl <tim.gangl30@gmail.com>
2020-02-29 10:59:33 -0800

    added font and some basic styling to homepage

--- client/src/components/Title/index.js
+++ client/src/components/Title/index.js
@@ -1,9 +1,9 @@
 import React from "react";
-
+import "./style.css";
 function Title() {
-    return (
-        <div className="jumbotron jumbotron-fluid"><h1>Welcome to setlist!</h1></div>
-    )
+  return (
+    <div className="jumbotron jumbotron-fluid"><h1>Welcome to setlist!</h1></div>
+  )
 }
 
 export default Title;
