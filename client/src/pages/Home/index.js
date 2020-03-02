162470b352892d8f025bfbb0b2404946ceb01469
Tim Gangl <tim.gangl30@gmail.com>
2020-03-01 17:51:15 -0800

    more styling/setlist component built

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -3,18 +3,28 @@ import Title from "../../components/Title";
 import UserLists from "../../components/UserLists";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import Calendar from "../../components/Calendar";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function Home() {
-    return (
-        <>
-            <Title />
-            <Row>
-                <Col xs={4}>
-                    <UserLists />
-                </Col>
-            </Row>
-        </>
-    )
+  return (
+    <>
+      <Title />
+      <Row>
+        <Col xs={6}>
+          <UserLists />
+        </Col>
+        <Col xs={6}>
+          <Row>
+            <Col xs={12}></Col>
+          </Row>
+          <Row>
+            <Col xs={12}><Calendar></Calendar></Col>
+          </Row>
+        </Col>
+      </Row>
+    </>
+  )
 }
 
 export default Home;
