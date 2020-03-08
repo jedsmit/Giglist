a59b0a61072da68b347cec45617f00fcf44c8eee
Tim Gangl <tim.gangl30@gmail.com>
2020-03-08 12:56:21 -0700

    styling/ added songs to seed/ parsing flat & sharp icons

--- models/song.js
+++ models/song.js
@@ -2,10 +2,11 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const songSchema = new Schema({
-    title: { type: String, required: true },
-    artist: { type: String },
-    genre: { type: String },
-    bpm: { type: Number }
+  title: { type: String, required: true },
+  artist: { type: String },
+  genre: { type: String },
+  bpm: { type: Number },
+  keySig: { type: String }
 });
 
 const Song = mongoose.model("Song", songSchema);
